156be9681deb25c29c81d9a089167ed7e6ab337d
argha-mirinfo <argha@mirinfosys.com>
2025-02-06 22:51:14 +0600

    v1 added

--- Royal_London/login_redesign/v1/v1_home.js
+++ Royal_London/login_redesign/v1/v1_home.js
@@ -3,3 +3,13 @@ var utils = optimizely.get('utils');
 utils.waitForElement('#header .navbar-expand-lg .d-lg-none a[title="Customer Login"]').then(function (login_btn_mobile) {
     login_btn_mobile.innerText = "Customer login or register";
 });
+
+utils.observeSelector('#header .navbar-expand-lg .d-lg-none a[title="Customer Login"] , #header .login-container .d-lg-flex a[title="Customer Login"]', function (login_cta) {
+    login_cta.addEventListener('click', function () {
+        window['optimizely'] = window['optimizely'] || [];
+        window['optimizely'].push({
+            type: "event",
+            eventName: "customer_login_cta_",
+        });
+    });
+});
