bdad35eb23fab1405f5d142b79fa5456977f7f0d
argha-mirinfo <argha@mirinfosys.com>
2025-04-04 16:40:05 +0600

    added

--- admiral/start_travel_date/control/control.js
+++ admiral/start_travel_date/control/control.js
@@ -2,42 +2,24 @@ const utils = optimizely.get('utils');
 
 if (window.location.pathname == '/trip-details') {
 
-    utils.observeSelector('.trip-type-container .annual', function (annual_trip_btn) {
-        annual_trip_btn.addEventListener('click', function () {
-            utils.observeSelector('.section:has(.start-date) input[name="startDate"]', function (start_date_input) {
+    utils.observeSelector('.section:has(.start-date) input[name="startDate"]', function (start_date_input) {
 
-                start_date_input.classList.add("opti-start-date");
+        if (start_date_input && !start_date_input.dataset.observerInitialized) {
+            const observer = new MutationObserver(() => {
 
-                let opti_start_date = document.querySelector(".opti-start-date");
-
-                if (opti_start_date && !opti_start_date.dataset.observerInitialized) {
-                    const observer = new MutationObserver(() => {
-                        console.log("argha", document.getElementById('annual-btn').checked)
-
-                        if (opti_start_date.getAttribute("value") && opti_start_date.getAttribute("value").length == 10 && document.getElementById('annual-btn').checked) {
-                            console.log("Value attribute changed:", opti_start_date.getAttribute("value"));
-                            window['optimizely'] = window['optimizely'] || [];
-                            window['optimizely'].push({
-                                type: "event",
-                                eventName: "cover_start_date_complete",
-                            });
-                        }
+                if (start_date_input.getAttribute("value") && start_date_input.getAttribute("value").length == 10 && document.getElementById('annual-btn') && document.getElementById('annual-btn').checked && !start_date_input.parentElement.classList.contains('has-error')) {
+                    window['optimizely'] = window['optimizely'] || [];
+                    window['optimizely'].push({
+                        type: "event",
+                        eventName: "cover_start_date_complete",
                     });
-
-                    observer.observe(opti_start_date, { attributes: true, attributeFilter: ["value"] });
-
-                    opti_start_date.dataset.observerInitialized = "true";
                 }
-
             });
-        })
-    });
 
-    utils.observeSelector('.trip-type-container .single', function (single_trip_btn) {
-        single_trip_btn.addEventListener('click', function () {
-            utils.observeSelector('.section:has(.start-date) input[name="startDate"]', function (start_date_input) {
-                start_date_input.classList.remove('opti-start-date')
-            })
-        });
+            observer.observe(start_date_input, { attributes: true, attributeFilter: ["value"] });
+
+            start_date_input.dataset.observerInitialized = "true";
+        }
+
     });
 }
