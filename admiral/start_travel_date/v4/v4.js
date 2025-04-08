9ed62f940519ed2b75e47a3517c399b7370da82a
argha-mirinfo <argha@mirinfosys.com>
2025-04-08 14:57:39 +0600

    added

--- admiral/start_travel_date/v4/v4.js
+++ admiral/start_travel_date/v4/v4.js
@@ -2,6 +2,27 @@ const utils = optimizely.get('utils');
 
 if (window.location.pathname == '/trip-details') {
 
+    utils.observeSelector('.section:has(.start-date) input[name="startDate"]', function (start_date_input) {
+
+        if (start_date_input && !start_date_input.dataset.observerInitialized) {
+            const observer = new MutationObserver(() => {
+
+                if (start_date_input.getAttribute("value") && start_date_input.getAttribute("value").length == 10 && document.getElementById('annual-btn') && document.getElementById('annual-btn').checked && !start_date_input.parentElement.classList.contains('has-error')) {
+                    window['optimizely'] = window['optimizely'] || [];
+                    window['optimizely'].push({
+                        type: "event",
+                        eventName: "cover_start_date_complete",
+                    });
+                }
+            });
+
+            observer.observe(start_date_input, { attributes: true, attributeFilter: ["value"] });
+
+            start_date_input.dataset.observerInitialized = "true";
+        }
+
+    });
+
     utils.observeSelector('.trip-type-container .annual', function (annual_trip_btn) {
         annual_trip_btn.addEventListener('click', function () {
             utils.observeSelector('.section:has(.start-date) .control-label', function (label) {
