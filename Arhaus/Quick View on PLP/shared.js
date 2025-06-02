c9b06ac4d378c8f7996c9b2b300696d96b8151a1
argha-mirinfo <argha@mirinfosys.com>
2025-06-02 19:41:51 +0600

    added

--- Arhaus/Quick View on PLP/shared.js
+++ Arhaus/Quick View on PLP/shared.js
@@ -1,76 +1,51 @@
 const utils = optimizely.get('utils');
 
 utils.observeSelector('.opti-quick-view-modal .quick-view-product .custom-input--materials', function (element) {
-    if (!element.dataset.listenerAdded) {
-        element.addEventListener("click", function () {
-            window['optimizely'] = window['optimizely'] || [];
-            window['optimizely'].push({
-                type: "event",
-                eventName: "click__quick_view_engagement_",
-            });
+    element.addEventListener("click", function () {
+        window['optimizely'] = window['optimizely'] || [];
+        window['optimizely'].push({
+            type: "event",
+            eventName: "click__quick_view_engagement_",
         });
-
-        // Mark as added
-        element.dataset.listenerAdded = "true";
-    }
+    });
 });
 
 utils.observeSelector('.opti-quick-view-modal .quick-view-product button:has(.icon--swatches)', function (element) {
-    if (!element.dataset.listenerAdded) {
-        element.addEventListener("click", function () {
-            window['optimizely'] = window['optimizely'] || [];
-            window['optimizely'].push({
-                type: "event",
-                eventName: "click__quick_view_engagement_",
-            });
+    element.addEventListener("click", function () {
+        window['optimizely'] = window['optimizely'] || [];
+        window['optimizely'].push({
+            type: "event",
+            eventName: "click__quick_view_engagement_",
         });
-
-        // Mark as added
-        element.dataset.listenerAdded = "true";
-    }
+    });
 });
 
 utils.observeSelector('.opti-quick-view-modal .quick-view-product button:has(.icon--more)', function (element) {
-    if (!element.dataset.listenerAdded) {
-        element.addEventListener("click", function () {
-            window['optimizely'] = window['optimizely'] || [];
-            window['optimizely'].push({
-                type: "event",
-                eventName: "click__quick_view_engagement_",
-            });
+    element.addEventListener("click", function () {
+        window['optimizely'] = window['optimizely'] || [];
+        window['optimizely'].push({
+            type: "event",
+            eventName: "click__quick_view_engagement_",
         });
-
-        // Mark as added
-        element.dataset.listenerAdded = "true";
-    }
+    });
 });
 
 utils.observeSelector('.opti-quick-view-modal .quick-view-product .custom-input.custom-input--inside-text', function (element) {
-    if (!element.dataset.listenerAdded) {
-        element.addEventListener("click", function () {
-            window['optimizely'] = window['optimizely'] || [];
-            window['optimizely'].push({
-                type: "event",
-                eventName: "click__quick_view_engagement_",
-            });
+    element.addEventListener("click", function () {
+        window['optimizely'] = window['optimizely'] || [];
+        window['optimizely'].push({
+            type: "event",
+            eventName: "click__quick_view_engagement_",
         });
-
-        // Mark as added
-        element.dataset.listenerAdded = "true";
-    }
+    });
 });
 
 utils.observeSelector('.opti-quick-view-modal .quick-view-product .product__label-link', function (element) {
-    if (!element.dataset.listenerAdded) {
-        element.addEventListener("click", function () {
-            window['optimizely'] = window['optimizely'] || [];
-            window['optimizely'].push({
-                type: "event",
-                eventName: "click__quick_view_engagement_",
-            });
+    element.addEventListener("click", function () {
+        window['optimizely'] = window['optimizely'] || [];
+        window['optimizely'].push({
+            type: "event",
+            eventName: "click__quick_view_engagement_",
         });
-
-        // Mark as added
-        element.dataset.listenerAdded = "true";
-    }
+    });
 });
