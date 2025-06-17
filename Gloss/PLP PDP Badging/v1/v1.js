c47ab0cb6eef3c45246e2a838850b6b7a12729dc
argha-mirinfo <argha@mirinfosys.com>
2025-06-17 22:22:19 +0600

    added

---
+++ Gloss/PLP PDP Badging/v1/v1.js
@@ -0,0 +1,76 @@
+const utils = optimizely.get('utils');
+
+utils.observeSelector('article.pi.js-product-item', function (productTiles) {
+
+    const product_name_node = productTiles.querySelector('.pi__details .js-product-title .js-product-item-link');
+    const product_image = productTiles.querySelector('.js-product-image');
+
+    if (product_name_node && product_image) {
+        const product_name = product_name_node.innerText.trim();
+
+        if (product_name == "Glossier You EDP" || product_name == "Glossier You Rêve" || product_name == "Glossier You Doux" || product_name == "Glossier You Fleur" || product_name == "Boy Brow" || product_name == "Ultralip" || product_name == "Lash Slick" || product_name == "Cloud Paint" || product_name == "Balm Dotcom" || product_name == "Lip Line") {
+
+            const product_existing_badge_node = productTiles.querySelector('.pi__img-label:not(.hide)');
+
+            if (product_existing_badge_node) {
+                const product_existing_badge_text = product_existing_badge_node.querySelector('.pi__img-label-text').innerText.trim();
+
+                if (product_existing_badge_text != "NEW") {
+                    product_existing_badge_node.classList.add('hide');
+                    product_image.insertAdjacentHTML("afterbegin", `<div class="pi__img-label opti-new-badge">
+                    <span class="pi__img-label-text">
+                        Best seller
+                    </span>
+                </div>`);
+                }
+            } else {
+                product_image.insertAdjacentHTML("afterbegin", `<div class="pi__img-label opti-new-badge">
+                    <span class="pi__img-label-text">
+                        Best seller
+                    </span>
+                </div>`);
+            }
+        }
+
+        if (product_name == "Futuredew Solid" || product_name == "Super Bounce" || product_name == "Body Hero Exfoliating Bar" || product_name == "Milky Jelly Cleansing Bar" || product_name == "Priming Moisturizer Balance" || product_name == "Full Orbit") {
+
+            const product_existing_badge_node = productTiles.querySelector('.pi__img-label:not(.hide)');
+
+            if (product_existing_badge_node) {
+                const product_existing_badge_text = product_existing_badge_node.querySelector('.pi__img-label-text').innerText.trim();
+
+                if (product_existing_badge_text != "NEW") {
+                    product_existing_badge_node.classList.add('hide');
+                    product_image.insertAdjacentHTML("afterbegin", `<div class="pi__img-label opti-new-badge">
+                    <span class="pi__img-label-text">
+                        Top rated
+                    </span>
+                </div>`);
+                }
+            } else {
+                product_image.insertAdjacentHTML("afterbegin", `<div class="pi__img-label opti-new-badge">
+                    <span class="pi__img-label-text">
+                        Top rated
+                    </span>
+                </div>`);
+            }
+        }
+    }
+
+});
+
+
+utils.waitForElement('.pv-essential .pv-header').then(function (pdp_page_header) {
+    const product_name_node = pdp_page_header.querySelector('.pv-header__title');
+    if (product_name_node) {
+        const product_name = product_name_node.innerText.trim();
+
+        if (product_name == "Glossier You EDP" || product_name == "Glossier You Rêve" || product_name == "Glossier You Doux" || product_name == "Glossier You Fleur" || product_name == "Boy Brow" || product_name == "Ultralip" || product_name == "Lash Slick" || product_name == "Cloud Paint" || product_name == "Balm Dotcom" || product_name == "Lip Line") {
+            pdp_page_header.insertAdjacentHTML("beforeend", `<span class="pv-header__sale-tag opti-pdp-badge">Best seller</span>`);
+        }
+
+        if (product_name == "Futuredew Solid" || product_name == "Super Bounce" || product_name == "Body Hero Exfoliating Bar" || product_name == "Milky Jelly Cleansing Bar" || product_name == "Priming Moisturizer Balance" || product_name == "Full Orbit") {
+            pdp_page_header.insertAdjacentHTML("beforeend", `<span class="pv-header__sale-tag opti-pdp-badge">Top rated</span>`);
+        }
+    }
+});
