5f0ae74ab8be126f20844613be42b5da801724ce
argha-mirinfo <argha@mirinfosys.com>
2025-06-20 21:23:57 +0600

    added

--- admiral/Lower_Tire_remove_cover_page/v1/v1.js
+++ admiral/Lower_Tire_remove_cover_page/v1/v1.js
@@ -118,5 +118,65 @@ if (window.location.pathname == '/Admiral/cover' && sessionStorage.getItem('opti
         });
     }
 
+    utils.observeSelector('eui-tier', function (cover_page) {
+        if (sessionStorage.getItem('is_going_back_from_quote_page') == "true") {
+
+            utils.observeSelector('eui-tier eui-motor-tiers-table adm-hero .adm-hero__title', function (hero_title) {
+                hero_title.innerText = "We have 4 levels of cover";
+            });
+
+            utils.observeSelector('table[data-test="eui-motor-tier-select"] thead tr th:nth-of-type(2)', function (essential_table_header) {
+                essential_table_header.style.display = 'table-cell';
+            });
+
+            utils.observeSelector('table[data-test="eui-motor-tier-select"] thead tr th:nth-of-type(3)', function (admiral_table_header) {
+                admiral_table_header.style.display = 'table-cell';
+            });
+
+            utils.observeSelector('table[data-test="eui-motor-tier-select"] thead tr th:nth-of-type(4)', function (gold_table_header) {
+                gold_table_header.style.display = 'table-cell';
+            });
+
+            utils.observeSelector('table[data-test="eui-motor-tier-select"] thead tr .adm-control-table__row-header', function (payment_frequency_table_header) {
+                payment_frequency_table_header.style.gridColumn = 'span var(--adm-control-table-cols, 3)';
+            });
+
+            utils.observeSelector('table[data-test="eui-motor-tier-select"] tbody tr td:nth-of-type(1)', function (essential_table_body) {
+                essential_table_body.style.display = 'table-cell';
+            });
+
+            utils.observeSelector('table[data-test="eui-motor-tier-select"] tbody tr td:nth-of-type(2)', function (admiral_table_body) {
+                admiral_table_body.style.display = 'table-cell';
+            });
+
+            utils.observeSelector('table[data-test="eui-motor-tier-select"] tbody tr td:nth-of-type(3)', function (gold_table_body) {
+                gold_table_body.style.display = 'table-cell';
+            });
+
+            utils.observeSelector('table[data-test="eui-motor-tier-select"] tbody tr th', function (tbody_table_header) {
+                tbody_table_header.style.gridColumn = 'span var(--adm-control-table-cols, 3)';
+            });
+
+            utils.observeSelector('table[data-test="eui-motor-tier-select"] tfoot tr td:nth-of-type(1)', function (essential_table_foot) {
+                essential_table_foot.style.display = 'table-cell';
+            });
+
+            utils.observeSelector('table[data-test="eui-motor-tier-select"] tfoot tr td:nth-of-type(2)', function (admiral_table_foot) {
+                admiral_table_foot.style.display = 'table-cell';
+            });
+
+            utils.observeSelector('table[data-test="eui-motor-tier-select"] tfoot tr td:nth-of-type(3)', function (gold_table_foot) {
+                gold_table_foot.style.display = 'table-cell';
+            });
+
+        }
+    });
+
 }
+
+utils.observeSelector('eui-quote small[data-test="other-tier-options"] a', function (change_cover_cta) {
+    change_cover_cta.addEventListener('click', function () {
+        sessionStorage.setItem('is_going_back_from_quote_page', "true");
+    });
+});
 
