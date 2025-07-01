cee8bf27d49a1eee4ba878e157cd74ffd63796bc
argha-mirinfo <argha@mirinfosys.com>
2025-07-01 13:37:52 +0600

    watch added

--- Michaelhill/Menu Navigation/v1/v1.js
+++ Michaelhill/Menu Navigation/v1/v1.js
@@ -2381,6 +2381,14 @@ utils.waitForElement('nav .menu .menu__root-links > li:nth-of-type(4)').then(fun
 
     watch_menu.insertAdjacentHTML("beforeend", `<div class="menu-panels menu-panels--close opti-watch-type-panel">
     <style>
+        .opti-watch-type-panel .menu-panels__header-back {
+            gap: 16px !important;
+        }
+
+        .opti-watch-type-panel .menu-panels__header-back svg {
+            height: unset !important;
+        }
+
         .opti-watch-type-panel .menu-panels__panel {
             padding-top: 24px;
         }
@@ -2406,13 +2414,29 @@ utils.waitForElement('nav .menu .menu__root-links > li:nth-of-type(4)').then(fun
 
     <div class="menu-panels__header">
         <button class="menu-panels__header-close" title="Close">
-            <svg class="icons">
-                <use href="/_nuxt3/icons.DgK34huS.svg#cross"></use>
+            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <mask id="mask0_2345_3243" style="mask-type:alpha" maskUnits="userSpaceOnUse" x="0" y="0" width="24"
+                    height="24">
+                    <rect width="24" height="24" fill="#D9D9D9" />
+                </mask>
+                <g mask="url(#mask0_2345_3243)">
+                    <path
+                        d="M6.7077 18.6154L6 17.9077L11.6 12.3077L6 6.7077L6.7077 6L12.3077 11.6L17.9077 6L18.6154 6.7077L13.0154 12.3077L18.6154 17.9077L17.9077 18.6154L12.3077 13.0154L6.7077 18.6154Z"
+                        fill="#010101" />
+                </g>
             </svg>
         </button>
         <button class="menu-panels__header-back">
-            <svg class="icons">
-                <use href="/_nuxt3/icons.DgK34huS.svg#arrow-carousel-left"></use>
+            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <mask id="mask0_2345_3247" style="mask-type:alpha" maskUnits="userSpaceOnUse" x="0" y="0" width="24"
+                    height="24">
+                    <rect width="24" height="24" fill="#D9D9D9" />
+                </mask>
+                <g mask="url(#mask0_2345_3247)">
+                    <path
+                        d="M14.3077 17.6154L9 12.3077L14.3077 7L15.0154 7.7077L10.4154 12.3077L15.0154 16.9077L14.3077 17.6154Z"
+                        fill="#010101" />
+                </g>
             </svg>
             Shop by Type
         </button>
@@ -2445,6 +2469,14 @@ utils.waitForElement('nav .menu .menu__root-links > li:nth-of-type(4)').then(fun
 
     watch_menu.insertAdjacentHTML("beforeend", `<div class="menu-panels menu-panels--close opti-watch-color-panel">
     <style>
+        .opti-watch-color-panel .menu-panels__header-back {
+            gap: 16px !important;
+        }
+
+        .opti-watch-color-panel .menu-panels__header-back svg {
+            height: unset !important;
+        }
+
         .opti-watch-color-panel .menu-panels__panel {
             padding-top: 24px;
         }
@@ -2473,13 +2505,29 @@ utils.waitForElement('nav .menu .menu__root-links > li:nth-of-type(4)').then(fun
 
     <div class="menu-panels__header">
         <button class="menu-panels__header-close" title="Close">
-            <svg class="icons">
-                <use href="/_nuxt3/icons.DgK34huS.svg#cross"></use>
+            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <mask id="mask0_2345_3243" style="mask-type:alpha" maskUnits="userSpaceOnUse" x="0" y="0" width="24"
+                    height="24">
+                    <rect width="24" height="24" fill="#D9D9D9" />
+                </mask>
+                <g mask="url(#mask0_2345_3243)">
+                    <path
+                        d="M6.7077 18.6154L6 17.9077L11.6 12.3077L6 6.7077L6.7077 6L12.3077 11.6L17.9077 6L18.6154 6.7077L13.0154 12.3077L18.6154 17.9077L17.9077 18.6154L12.3077 13.0154L6.7077 18.6154Z"
+                        fill="#010101" />
+                </g>
             </svg>
         </button>
         <button class="menu-panels__header-back">
-            <svg class="icons">
-                <use href="/_nuxt3/icons.DgK34huS.svg#arrow-carousel-left"></use>
+            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <mask id="mask0_2345_3247" style="mask-type:alpha" maskUnits="userSpaceOnUse" x="0" y="0" width="24"
+                    height="24">
+                    <rect width="24" height="24" fill="#D9D9D9" />
+                </mask>
+                <g mask="url(#mask0_2345_3247)">
+                    <path
+                        d="M14.3077 17.6154L9 12.3077L14.3077 7L15.0154 7.7077L10.4154 12.3077L15.0154 16.9077L14.3077 17.6154Z"
+                        fill="#010101" />
+                </g>
             </svg>
             Shop by Colour
         </button>
@@ -2585,15 +2633,6 @@ utils.waitForElement('nav .menu .menu__root-links > li:nth-of-type(4)').then(fun
             padding: 16px 0;
         }
 
-        .opti-new-watch-menu .bold-menu .sub {
-            font-weight: 400;
-            color: #707172;
-        }
-
-        .opti-new-watch-menu .bold-menu .icons {
-            height: 1rem;
-        }
-
         .opti-new-watch-menu .menu-heading {
             font-size: 12px;
             line-height: 1.2;
@@ -2639,15 +2678,29 @@ utils.waitForElement('nav .menu .menu__root-links > li:nth-of-type(4)').then(fun
 
     <div class="bold-menu type">
         Shop by Type
-        <svg class="icons">
-            <use href="/_nuxt3/icons.DgK34huS.svg#arrow-carousel-right"></use>
+        <svg width="24" height="25" viewBox="0 0 24 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <mask id="mask0_2345_2756" style="mask-type:alpha" maskUnits="userSpaceOnUse" x="0" y="0" width="24"
+                height="25">
+                <rect y="0.746094" width="24" height="24" fill="#D9D9D9" />
+            </mask>
+            <g mask="url(#mask0_2345_2756)">
+                <path d="M13.6 13.0538L9 8.45379L9.7077 7.74609L15.0154 13.0538L9.7077 18.3615L9 17.6538L13.6 13.0538Z"
+                    fill="#010101" />
+            </g>
         </svg>
     </div>
 
     <div class="bold-menu color">
         Shop by Colour
-        <svg class="icons">
-            <use href="/_nuxt3/icons.DgK34huS.svg#arrow-carousel-right"></use>
+        <svg width="24" height="25" viewBox="0 0 24 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <mask id="mask0_2345_2756" style="mask-type:alpha" maskUnits="userSpaceOnUse" x="0" y="0" width="24"
+                height="25">
+                <rect y="0.746094" width="24" height="24" fill="#D9D9D9" />
+            </mask>
+            <g mask="url(#mask0_2345_2756)">
+                <path d="M13.6 13.0538L9 8.45379L9.7077 7.74609L15.0154 13.0538L9.7077 18.3615L9 17.6538L13.6 13.0538Z"
+                    fill="#010101" />
+            </g>
         </svg>
     </div>
 </div>`);
