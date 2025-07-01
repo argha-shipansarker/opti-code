231a63d163182432c4115ca751298246ccf4b8a9
argha-mirinfo <argha@mirinfosys.com>
2025-07-01 13:08:05 +0600

    engagement updated

--- Michaelhill/Menu Navigation/v1/v1.js
+++ Michaelhill/Menu Navigation/v1/v1.js
@@ -62,6 +62,14 @@ utils.waitForElement('nav .menu .menu__root-links > li:nth-of-type(1)').then(fun
 
     engagement_menu.insertAdjacentHTML("beforeend", `<div class="menu-panels menu-panels--close opti-engagement-cut-panels">
     <style>
+        .opti-engagement-cut-panels .menu-panels__header-back {
+            gap: 16px !important;
+        }
+
+        .opti-engagement-cut-panels .menu-panels__header-back svg {
+            height: unset !important;
+        }
+
         .opti-engagement-cut-panels .menu-panels__panel {
             display: flex;
             flex-wrap: wrap;
@@ -93,13 +101,29 @@ utils.waitForElement('nav .menu .menu__root-links > li:nth-of-type(1)').then(fun
 
     <div class="menu-panels__header">
         <button class="menu-panels__header-close" title="Close">
-            <svg class="icons">
-                <use href="/_nuxt3/icons.DgK34huS.svg#cross"></use>
+            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <mask id="mask0_2345_3243" style="mask-type:alpha" maskUnits="userSpaceOnUse" x="0" y="0" width="24"
+                    height="24">
+                    <rect width="24" height="24" fill="#D9D9D9" />
+                </mask>
+                <g mask="url(#mask0_2345_3243)">
+                    <path
+                        d="M6.7077 18.6154L6 17.9077L11.6 12.3077L6 6.7077L6.7077 6L12.3077 11.6L17.9077 6L18.6154 6.7077L13.0154 12.3077L18.6154 17.9077L17.9077 18.6154L12.3077 13.0154L6.7077 18.6154Z"
+                        fill="#010101" />
+                </g>
             </svg>
         </button>
         <button class="menu-panels__header-back">
-            <svg class="icons">
-                <use href="/_nuxt3/icons.DgK34huS.svg#arrow-carousel-left"></use>
+            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <mask id="mask0_2345_3247" style="mask-type:alpha" maskUnits="userSpaceOnUse" x="0" y="0" width="24"
+                    height="24">
+                    <rect width="24" height="24" fill="#D9D9D9" />
+                </mask>
+                <g mask="url(#mask0_2345_3247)">
+                    <path
+                        d="M14.3077 17.6154L9 12.3077L14.3077 7L15.0154 7.7077L10.4154 12.3077L15.0154 16.9077L14.3077 17.6154Z"
+                        fill="#010101" />
+                </g>
             </svg>
             Cut
         </button>
@@ -560,6 +584,14 @@ utils.waitForElement('nav .menu .menu__root-links > li:nth-of-type(1)').then(fun
 
     engagement_menu.insertAdjacentHTML("beforeend", `<div class="menu-panels menu-panels--close opti-engagement-bridal-collections">
     <style>
+        .opti-engagement-bridal-collections .menu-panels__header-back {
+            gap: 16px !important;
+        }
+
+        .opti-engagement-bridal-collections .menu-panels__header-back svg {
+            height: unset !important;
+        }
+
         .opti-engagement-bridal-collections .menu-panels__header {
             border-bottom: 0;
         }
@@ -594,13 +626,29 @@ utils.waitForElement('nav .menu .menu__root-links > li:nth-of-type(1)').then(fun
 
     <div class="menu-panels__header">
         <button class="menu-panels__header-close" title="Close">
-            <svg class="icons">
-                <use href="/_nuxt3/icons.DgK34huS.svg#cross"></use>
+            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <mask id="mask0_2345_3243" style="mask-type:alpha" maskUnits="userSpaceOnUse" x="0" y="0" width="24"
+                    height="24">
+                    <rect width="24" height="24" fill="#D9D9D9" />
+                </mask>
+                <g mask="url(#mask0_2345_3243)">
+                    <path
+                        d="M6.7077 18.6154L6 17.9077L11.6 12.3077L6 6.7077L6.7077 6L12.3077 11.6L17.9077 6L18.6154 6.7077L13.0154 12.3077L18.6154 17.9077L17.9077 18.6154L12.3077 13.0154L6.7077 18.6154Z"
+                        fill="#010101" />
+                </g>
             </svg>
         </button>
         <button class="menu-panels__header-back">
-            <svg class="icons">
-                <use href="/_nuxt3/icons.DgK34huS.svg#arrow-carousel-left"></use>
+            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <mask id="mask0_2345_3247" style="mask-type:alpha" maskUnits="userSpaceOnUse" x="0" y="0" width="24"
+                    height="24">
+                    <rect width="24" height="24" fill="#D9D9D9" />
+                </mask>
+                <g mask="url(#mask0_2345_3247)">
+                    <path
+                        d="M14.3077 17.6154L9 12.3077L14.3077 7L15.0154 7.7077L10.4154 12.3077L15.0154 16.9077L14.3077 17.6154Z"
+                        fill="#010101" />
+                </g>
             </svg>
             Bridal Collections
         </button>
@@ -719,15 +767,29 @@ utils.waitForElement('nav .menu .menu__root-links > li:nth-of-type(1)').then(fun
 
     <div class="bold-menu cut">
         <span>Cut <span class="sub">(Shape)</span></span>
-        <svg class="icons">
-            <use href="/_nuxt3/icons.DgK34huS.svg#arrow-carousel-right"></use>
+        <svg width="24" height="25" viewBox="0 0 24 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <mask id="mask0_2345_2756" style="mask-type:alpha" maskUnits="userSpaceOnUse" x="0" y="0" width="24"
+                height="25">
+                <rect y="0.746094" width="24" height="24" fill="#D9D9D9" />
+            </mask>
+            <g mask="url(#mask0_2345_2756)">
+                <path d="M13.6 13.0538L9 8.45379L9.7077 7.74609L15.0154 13.0538L9.7077 18.3615L9 17.6538L13.6 13.0538Z"
+                    fill="#010101" />
+            </g>
         </svg>
     </div>
 
     <div class="bold-menu bridal-collection">
         Bridal Collections
-        <svg class="icons">
-            <use href="/_nuxt3/icons.DgK34huS.svg#arrow-carousel-right"></use>
+        <svg width="24" height="25" viewBox="0 0 24 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <mask id="mask0_2345_2756" style="mask-type:alpha" maskUnits="userSpaceOnUse" x="0" y="0" width="24"
+                height="25">
+                <rect y="0.746094" width="24" height="24" fill="#D9D9D9" />
+            </mask>
+            <g mask="url(#mask0_2345_2756)">
+                <path d="M13.6 13.0538L9 8.45379L9.7077 7.74609L15.0154 13.0538L9.7077 18.3615L9 17.6538L13.6 13.0538Z"
+                    fill="#010101" />
+            </g>
         </svg>
     </div>
 
