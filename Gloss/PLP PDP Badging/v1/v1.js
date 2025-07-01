2fb5c7e916b3b8a65fe5be02e67733f0360cf3ad
argha-mirinfo <argha@mirinfosys.com>
2025-07-01 22:01:55 +0600

    added

--- Gloss/PLP PDP Badging/v1/v1.js
+++ Gloss/PLP PDP Badging/v1/v1.js
@@ -12,18 +12,7 @@ utils.observeSelector('article.pi.js-product-item', function (productTiles) {
 
             const product_existing_badge_node = productTiles.querySelector('.pi__img-label:not(.hide)');
 
-            if (product_existing_badge_node) {
-                const product_existing_badge_text = product_existing_badge_node.querySelector('.pi__img-label-text').innerText.trim();
-
-                if (product_existing_badge_text != "NEW") {
-                    product_existing_badge_node.classList.add('hide');
-                    product_image.insertAdjacentHTML("afterbegin", `<div class="pi__img-label opti-new-badge">
-                    <span class="pi__img-label-text">
-                        Best seller
-                    </span>
-                </div>`);
-                }
-            } else {
+            if (!product_existing_badge_node) {
                 product_image.insertAdjacentHTML("afterbegin", `<div class="pi__img-label opti-new-badge">
                     <span class="pi__img-label-text">
                         Best seller
@@ -36,18 +25,7 @@ utils.observeSelector('article.pi.js-product-item', function (productTiles) {
 
             const product_existing_badge_node = productTiles.querySelector('.pi__img-label:not(.hide)');
 
-            if (product_existing_badge_node) {
-                const product_existing_badge_text = product_existing_badge_node.querySelector('.pi__img-label-text').innerText.trim();
-
-                if (product_existing_badge_text != "NEW") {
-                    product_existing_badge_node.classList.add('hide');
-                    product_image.insertAdjacentHTML("afterbegin", `<div class="pi__img-label opti-new-badge">
-                    <span class="pi__img-label-text">
-                        Top rated
-                    </span>
-                </div>`);
-                }
-            } else {
+            if (!product_existing_badge_node) {
                 product_image.insertAdjacentHTML("afterbegin", `<div class="pi__img-label opti-new-badge">
                     <span class="pi__img-label-text">
                         Top rated
@@ -62,7 +40,9 @@ utils.observeSelector('article.pi.js-product-item', function (productTiles) {
 
 utils.waitForElement('.pv-essential .pv-header').then(function (pdp_page_header) {
     const product_name_node = pdp_page_header.querySelector('.pv-header__title');
-    if (product_name_node) {
+    const product_existing_badge_number = pdp_page_header.querySelectorAll('.pv-header__sale-tag:not(.hide)');
+
+    if (product_name_node && product_existing_badge_number.length < 2) {
         const product_name = product_name_node.innerText.trim();
 
         if (product_name == "Glossier You" || product_name == "Glossier You Rêve" || product_name == "Glossier You Doux" || product_name == "Glossier You Fleur" || product_name == "Boy Brow" || product_name == "Ultralip" || product_name == "Lash Slick" || product_name == "Cloud Paint" || product_name == "Balm Dotcom" || product_name == "Lip Line") {
