94a8beac65b6a18b83b2c75748e1cb1581ae9e95
argha-mirinfo <argha@mirinfosys.com>
2025-07-10 16:00:38 +0600

    added

--- Michaelhill/Menu Navigation/v1/v1.js
+++ Michaelhill/Menu Navigation/v1/v1.js
@@ -5235,5 +5235,415 @@ utils.observeSelector('.header-menu-drawer .header-menu-drawer__panel-switcher-w
 
     if (window.opti_selected_menu_name == "JEWELLERY") {
         vue_menu_2nd_level_container.classList.add('opti-jewellery-vue-menu-panel');
+
+        vue_menu_2nd_level_container.insertAdjacentHTML("afterbegin", `<style>
+    .header-menu-drawer .header-menu-drawer__close-button {
+        background: #FFFFFF !important;
+    }
+
+
+    .header-menu-drawer .header-menu-drawer__panel-switcher-wrapper .header-menu-drawer__panel-switcher-link-content {
+        border-bottom: 1px solid #E8E9EA;
+    }
+
+    .header-menu-drawer .header-menu-drawer__panel-switcher-wrapper .navigation-section-panel .navigation-section-panel__grid .navigation-section-panel__grid-unit:nth-of-type(1) {
+        padding: 36px 0 0px !important;
+        border-bottom: 0px;
+    }
+
+    .header-menu-drawer .header-menu-drawer__panel-switcher-wrapper .navigation-section-panel .navigation-section-panel__grid .navigation-section-panel__grid-unit:nth-of-type(1) a {
+        font-size: 16px;
+    }
+
+    .header-menu-drawer .header-menu-drawer__panel-switcher-wrapper .navigation-section-panel .navigation-section-panel__grid .navigation-section-panel__grid-unit:nth-of-type(1) .navigation-utility-panel .navigation-utility-panel__links li:nth-of-type(8),
+    .header-menu-drawer .header-menu-drawer__panel-switcher-wrapper .navigation-section-panel .navigation-section-panel__grid .navigation-section-panel__grid-unit:nth-of-type(1) .navigation-section-panel__header-link {
+        display: none;
+    }
+
+    .header-menu-drawer .header-menu-drawer__panel-switcher-wrapper .navigation-section-panel .navigation-section-panel__grid .navigation-section-panel__grid-unit:nth-of-type(1) .navigation-utility-panel .navigation-utility-panel__links li:nth-of-type(10) {
+        margin-bottom: 12px;
+    }
+
+    .header-menu-drawer .header-menu-drawer__panel-switcher-wrapper .navigation-section-panel .navigation-section-panel__grid .navigation-section-panel__grid-unit:nth-of-type(3),
+    .header-menu-drawer .header-menu-drawer__panel-switcher-wrapper .navigation-section-panel .navigation-section-panel__grid .navigation-section-panel__grid-unit:nth-of-type(4) {
+        display: none;
+    }
+
+    .header-menu-drawer .header-menu-drawer__panel-switcher-wrapper .navigation-section-panel .navigation-section-panel__grid .navigation-section-panel__grid-unit:nth-of-type(5) .optimised-picture-wrapper {
+        display: none !important;
+    }
+
+    .header-menu-drawer .header-menu-drawer__panel-switcher-wrapper .navigation-section-panel .navigation-section-panel__grid .navigation-section-panel__grid-unit:nth-of-type(5) p {
+        font-size: 16px;
+    }
+
+    .header-menu-drawer .header-menu-drawer__panel-switcher-wrapper .navigation-section-panel .navigation-section-panel__grid .navigation-section-panel__grid-unit:nth-of-type(5) p a {
+        font-size: 16px;
+        text-decoration: underline;
+        color: #282829;
+        font-weight: 400;
+    }
+
+    .opti-jewellery-gemstone-heading.close {
+        display: none;
+    }
+
+    .opti-jewellery-gemstone-heading.open {
+        display: flex;
+    }
+
+    .opti-jewellery-gemstone-panel.close {
+        display: none;
+    }
+
+    .opti-jewellery-gemstone-panel.open {
+        display: block;
+    }
+
+    .opti-jewellery-gemstone-heading {
+        padding-bottom: 24px;
+    }
+
+    .opti-jewellery-gemstone-heading {
+        border-bottom: 1px solid #E8E9EA;
+    }
+
+    .opti-jewellery-gemstone-heading p {
+        font-family: "Söhne-Kräftig", Arial, Helvetica, sans-serif;
+        font-size: 16px;
+        line-height: 24px;
+        margin: 0 0 0 16px !important;
+    }
+
+    .opti-jewellery-gemstone-panel {
+        box-sizing: border-box;
+        max-height: calc(100vh - 128px);
+        overflow: scroll;
+        scrollbar-width: none;
+        width: 100%;
+        margin: 0;
+        padding: 0;
+    }
+
+    .opti-jewellery-gemstone-panel .menu-panels__panel {
+        padding-top: 24px;
+    }
+
+    .opti-jewellery-gemstone-panel .menu-panels__panel .menu-items {
+        display: flex;
+        padding: 12.5px 0;
+        align-items: center;
+        text-decoration: none;
+    }
+
+    .opti-jewellery-gemstone-panel .menu-panels__panel .menu-items p {
+        font-size: 16px;
+        line-height: 1.2;
+        letter-spacing: 0.02rem;
+        font-weight: 400;
+        margin-bottom: 0 !important;
+        color: #282829;
+        margin-left: 8px;
+    }
+
+    .opti-jewellery-gemstone-panel .menu-panels__panel .menu-items.all p {
+        font-weight: 700;
+        margin-left: 0px;
+    }
+</style>`);
+
+        vue_menu_2nd_level_container.insertAdjacentHTML("beforeend", `<div class="opti-jewellery-gemstone-heading close">
+    <button>
+        <svg data-v-ce454e80="" width="24" height="24" viewBox="0 0 24 24" fill="none"
+            xmlns="http://www.w3.org/2000/svg" class="">
+            <mask data-v-ce454e80="" id="mask0_3820_13683" maskUnits="userSpaceOnUse" x="0" y="0" width="24" height="24"
+                style="mask-type: alpha;">
+                <rect data-v-ce454e80="" width="24" height="24" fill="#D9D9D9"></rect>
+            </mask>
+            <g data-v-ce454e80="" mask="url(#mask0_3820_13683)">
+                <path data-v-ce454e80=""
+                    d="M14.3077 17.6154L9 12.3077L14.3077 7L15.0154 7.7077L10.4154 12.3077L15.0154 16.9077L14.3077 17.6154Z"
+                    fill="#010101"></path>
+            </g>
+        </svg>
+    </button>
+    <p>
+        Gemstones
+    </p>
+</div>
+
+<div class="opti-jewellery-gemstone-panel close">
+
+    <div class="menu-panels__panel">
+
+        <a href="/jewellery/gemstones" class="menu-items all">
+            <p>All Gemstones</p>
+        </a>
+
+        <a href="/jewellery/gemstones/aquamarine" class="menu-items">
+            <svg width="20" height="21" viewBox="0 0 20 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path
+                    d="M10 20.8984C15.5228 20.8984 20 16.4213 20 10.8984C20 5.37559 15.5228 0.898438 10 0.898438C4.47715 0.898438 0 5.37559 0 10.8984C0 16.4213 4.47715 20.8984 10 20.8984Z"
+                    fill="#BEC7C7" />
+                <rect x="2.92871" y="10.8984" width="10" height="10" transform="rotate(-45 2.92871 10.8984)"
+                    fill="#E8F1F3" />
+            </svg>
+            <p>Aquamarine</p>
+        </a>
+
+        <a href="/diamonds/diamond-jewellery" class="menu-items">
+            <svg width="20" height="21" viewBox="0 0 20 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path
+                    d="M10 20.8984C15.5228 20.8984 20 16.4213 20 10.8984C20 5.37559 15.5228 0.898438 10 0.898438C4.47715 0.898438 0 5.37559 0 10.8984C0 16.4213 4.47715 20.8984 10 20.8984Z"
+                    fill="#E5E5E2" />
+                <rect x="2.92871" y="10.8984" width="10" height="10" transform="rotate(-45 2.92871 10.8984)"
+                    fill="#FEFEFE" />
+            </svg>
+            <p>Diamonds</p>
+        </a>
+
+        <a href="/jewellery/gemstones/pearl" class="menu-items">
+            <svg width="20" height="21" viewBox="0 0 20 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path
+                    d="M10 20.8984C15.5228 20.8984 20 16.4213 20 10.8984C20 5.37559 15.5228 0.898438 10 0.898438C4.47715 0.898438 0 5.37559 0 10.8984C0 16.4213 4.47715 20.8984 10 20.8984Z"
+                    fill="#EAE8E7" />
+                <rect x="10" y="3.89844" width="10" height="10" rx="5" transform="rotate(45 10 3.89844)"
+                    fill="#FBFAF8" />
+            </svg>
+            <p>Pearls</p>
+        </a>
+
+        <a href="/jewellery/gemstones/emerald" class="menu-items">
+            <svg width="20" height="21" viewBox="0 0 20 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path
+                    d="M10 20.8984C15.5228 20.8984 20 16.4213 20 10.8984C20 5.37559 15.5228 0.898438 10 0.898438C4.47715 0.898438 0 5.37559 0 10.8984C0 16.4213 4.47715 20.8984 10 20.8984Z"
+                    fill="#005835" />
+                <rect x="2.92871" y="10.8984" width="10" height="10" transform="rotate(-45 2.92871 10.8984)"
+                    fill="#028F56" />
+            </svg>
+            <p>Emerald</p>
+        </a>
+
+        <a href="/jewellery/gemstones/garnet" class="menu-items">
+            <svg width="20" height="21" viewBox="0 0 20 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path
+                    d="M10 20.8984C15.5228 20.8984 20 16.4213 20 10.8984C20 5.37559 15.5228 0.898438 10 0.898438C4.47715 0.898438 0 5.37559 0 10.8984C0 16.4213 4.47715 20.8984 10 20.8984Z"
+                    fill="#552620" />
+                <rect x="2.92871" y="10.8984" width="10" height="10" transform="rotate(-45 2.92871 10.8984)"
+                    fill="#E94531" />
+            </svg>
+            <p>Garnet</p>
+        </a>
+
+        <a href="/jewellery/gemstones/morganite" class="menu-items">
+            <svg width="20" height="21" viewBox="0 0 20 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path
+                    d="M10 20.8984C15.5228 20.8984 20 16.4213 20 10.8984C20 5.37559 15.5228 0.898438 10 0.898438C4.47715 0.898438 0 5.37559 0 10.8984C0 16.4213 4.47715 20.8984 10 20.8984Z"
+                    fill="#D1AC96" />
+                <rect x="2.92871" y="10.8984" width="10" height="10" transform="rotate(-45 2.92871 10.8984)"
+                    fill="#FAECDD" />
+                <path
+                    d="M10 20.8984C15.5228 20.8984 20 16.4213 20 10.8984C20 5.37559 15.5228 0.898438 10 0.898438C4.47715 0.898438 0 5.37559 0 10.8984C0 16.4213 4.47715 20.8984 10 20.8984Z"
+                    fill="#D1AC96" />
+                <rect x="10" y="3.89844" width="10.1011" height="10.1011" transform="rotate(45 10 3.89844)"
+                    fill="#FAECDD" />
+            </svg>
+            <p>Morganite</p>
+        </a>
+
+        <a href="/jewellery/gemstones/opal" class="menu-items">
+            <svg width="20" height="21" viewBox="0 0 20 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path
+                    d="M10 20.8984C15.5228 20.8984 20 16.4213 20 10.8984C20 5.37559 15.5228 0.898438 10 0.898438C4.47715 0.898438 0 5.37559 0 10.8984C0 16.4213 4.47715 20.8984 10 20.8984Z"
+                    fill="#EFF1EB" />
+                <rect x="10" y="3.89844" width="10" height="10" rx="5" transform="rotate(45 10 3.89844)"
+                    fill="#EBE4DA" />
+            </svg>
+            <p>Opal</p>
+        </a>
+
+        <a href="/jewellery/gemstones/peridot" class="menu-items">
+            <svg width="20" height="21" viewBox="0 0 20 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path
+                    d="M10 20.8984C15.5228 20.8984 20 16.4213 20 10.8984C20 5.37559 15.5228 0.898438 10 0.898438C4.47715 0.898438 0 5.37559 0 10.8984C0 16.4213 4.47715 20.8984 10 20.8984Z"
+                    fill="#9E9E54" />
+                <rect x="2.92871" y="10.8984" width="10" height="10" transform="rotate(-45 2.92871 10.8984)"
+                    fill="#C3CD6F" />
+            </svg>
+            <p>Peridot</p>
+        </a>
+
+        <a href="/jewellery/gemstones/ruby" class="menu-items">
+            <svg width="20" height="21" viewBox="0 0 20 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path
+                    d="M10 20.8984C15.5228 20.8984 20 16.4213 20 10.8984C20 5.37559 15.5228 0.898438 10 0.898438C4.47715 0.898438 0 5.37559 0 10.8984C0 16.4213 4.47715 20.8984 10 20.8984Z"
+                    fill="#341B1F" />
+                <rect x="2.92871" y="10.8984" width="10" height="10" transform="rotate(-45 2.92871 10.8984)"
+                    fill="#C60158" />
+            </svg>
+            <p>Ruby</p>
+        </a>
+
+        <a href="/jewellery/gemstones/sapphire" class="menu-items">
+            <svg width="20" height="21" viewBox="0 0 20 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path
+                    d="M10 20.8984C15.5228 20.8984 20 16.4213 20 10.8984C20 5.37559 15.5228 0.898438 10 0.898438C4.47715 0.898438 0 5.37559 0 10.8984C0 16.4213 4.47715 20.8984 10 20.8984Z"
+                    fill="#181C44" />
+                <rect x="2.92871" y="10.8984" width="10" height="10" transform="rotate(-45 2.92871 10.8984)"
+                    fill="#2F4B93" />
+            </svg>
+            <p>Sapphire</p>
+        </a>
+
+        <a href="/jewellery/gemstones/tanzanite" class="menu-items">
+            <svg width="20" height="21" viewBox="0 0 20 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path
+                    d="M10 20.8984C15.5228 20.8984 20 16.4213 20 10.8984C20 5.37559 15.5228 0.898438 10 0.898438C4.47715 0.898438 0 5.37559 0 10.8984C0 16.4213 4.47715 20.8984 10 20.8984Z"
+                    fill="#88B1EB" />
+                <rect x="2.92871" y="10.8984" width="10" height="10" transform="rotate(-45 2.92871 10.8984)"
+                    fill="#C3DBF1" />
+            </svg>
+            <p>Tanzanite</p>
+        </a>
+
+        <a href="/jewellery/gemstones/tourmaline" class="menu-items">
+            <svg width="20" height="21" viewBox="0 0 20 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path
+                    d="M10 20.8984C15.5228 20.8984 20 16.4213 20 10.8984C20 5.37559 15.5228 0.898438 10 0.898438C4.47715 0.898438 0 5.37559 0 10.8984C0 16.4213 4.47715 20.8984 10 20.8984Z"
+                    fill="#88B1EB" />
+                <rect x="2.92871" y="10.8984" width="10" height="10" transform="rotate(-45 2.92871 10.8984)"
+                    fill="#C3DBF1" />
+            </svg>
+            <p>Tourmaline</p>
+        </a>
+    </div>
+
+</div>`);
+
+        first_grid_menu = vue_menu_2nd_level_container.querySelector('.navigation-section-panel .navigation-section-panel__grid .navigation-section-panel__grid-unit:nth-of-type(1)');
+
+        first_grid_menu.insertAdjacentHTML("afterend", `<div class="opti-new-jewellery-menu">
+    <style>
+        .opti-new-jewellery-menu .bold-menu {
+            font-size: 16px;
+            font-weight: 700;
+            line-height: 1.2;
+            letter-spacing: 0.02rem;
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            color: #282829;
+            text-decoration: none;
+            padding: 18.5px 0;
+            border-bottom: 1px solid #E8E9EA;
+        }
+
+        .opti-new-jewellery-menu a.bold-menu {
+            padding: 16px 0;
+        }
+
+        .opti-new-jewellery-menu .bold-menu.explore:hover {
+            text-decoration: underline;
+            color: #4c6335;
+        }
+    </style>
+
+    <div class="bold-menu gemstones">
+        Gemstones
+        <svg width="24" height="25" viewBox="0 0 24 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <mask id="mask0_2345_2756" style="mask-type:alpha" maskUnits="userSpaceOnUse" x="0" y="0" width="24"
+                height="25">
+                <rect y="0.746094" width="24" height="24" fill="#D9D9D9" />
+            </mask>
+            <g mask="url(#mask0_2345_2756)">
+                <path d="M13.6 13.0538L9 8.45379L9.7077 7.74609L15.0154 13.0538L9.7077 18.3615L9 17.6538L13.6 13.0538Z"
+                    fill="#010101" />
+            </g>
+        </svg>
+    </div>
+
+    <div class="bold-menu metals">
+        Metals
+        <svg width="24" height="25" viewBox="0 0 24 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <mask id="mask0_2345_2756" style="mask-type:alpha" maskUnits="userSpaceOnUse" x="0" y="0" width="24"
+                height="25">
+                <rect y="0.746094" width="24" height="24" fill="#D9D9D9" />
+            </mask>
+            <g mask="url(#mask0_2345_2756)">
+                <path d="M13.6 13.0538L9 8.45379L9.7077 7.74609L15.0154 13.0538L9.7077 18.3615L9 17.6538L13.6 13.0538Z"
+                    fill="#010101" />
+            </g>
+        </svg>
+    </div>
+
+    <div class="bold-menu collections">
+        Collections
+        <svg width="24" height="25" viewBox="0 0 24 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <mask id="mask0_2345_2756" style="mask-type:alpha" maskUnits="userSpaceOnUse" x="0" y="0" width="24"
+                height="25">
+                <rect y="0.746094" width="24" height="24" fill="#D9D9D9" />
+            </mask>
+            <g mask="url(#mask0_2345_2756)">
+                <path d="M13.6 13.0538L9 8.45379L9.7077 7.74609L15.0154 13.0538L9.7077 18.3615L9 17.6538L13.6 13.0538Z"
+                    fill="#010101" />
+            </g>
+        </svg>
+    </div>
+
+    <div class="bold-menu personalised-jewellery">
+        Personalised Jewellery
+        <svg width="24" height="25" viewBox="0 0 24 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <mask id="mask0_2345_2756" style="mask-type:alpha" maskUnits="userSpaceOnUse" x="0" y="0" width="24"
+                height="25">
+                <rect y="0.746094" width="24" height="24" fill="#D9D9D9" />
+            </mask>
+            <g mask="url(#mask0_2345_2756)">
+                <path d="M13.6 13.0538L9 8.45379L9.7077 7.74609L15.0154 13.0538L9.7077 18.3615L9 17.6538L13.6 13.0538Z"
+                    fill="#010101" />
+            </g>
+        </svg>
+    </div>
+
+    <a href="/jewellery" class="bold-menu explore">
+        Explore Jewellery
+    </a>
+</div>`);
+
+        const jewellery_menu_panel_first_section = vue_menu_2nd_level_container.querySelector('.navigation-section-panel .navigation-section-panel__grid .navigation-section-panel__grid-unit:nth-of-type(1) .navigation-utility-panel');
+        const list = jewellery_menu_panel_first_section.querySelector('ul');
+
+        const tennis_menu = list.children[5];
+        const chains_menu = list.children[6];
+
+        if (tennis_menu && chains_menu && list) {
+            list.insertBefore(chains_menu, tennis_menu);
+        }
+
+        const shop_all_jewellery = vue_menu_2nd_level_container.querySelector('.navigation-section-panel .navigation-section-panel__grid .navigation-section-panel__grid-unit:nth-of-type(1) .navigation-utility-panel .navigation-utility-panel__links li:nth-of-type(10) a')
+        if (shop_all_jewellery) {
+            shop_all_jewellery.href = '/jewellery';
+        }
+
+        bindVueMenuToggleClick({
+            triggerSelector: '.opti-new-jewellery-menu .gemstones',
+            isOpening: true,
+            headingSelector: '.opti-jewellery-gemstone-heading',
+            collectionsSelector: '.opti-jewellery-gemstone-panel',
+            container: vue_menu_2nd_level_container
+        });
+
+        bindVueMenuToggleClick({
+            triggerSelector: '.opti-jewellery-gemstone-heading',
+            isOpening: false,
+            headingSelector: '.opti-jewellery-gemstone-heading',
+            collectionsSelector: '.opti-jewellery-gemstone-panel',
+            container: vue_menu_2nd_level_container
+        });
+
+        const jewellery_menu_fourth_section_image_container = vue_menu_2nd_level_container.querySelector('.navigation-section-panel .navigation-section-panel__grid .navigation-section-panel__grid-unit:nth-of-type(5) .optimised-picture-wrapper');
+
+        if (jewellery_menu_fourth_section_image_container) {
+            jewellery_menu_fourth_section_image_container.insertAdjacentHTML("afterend", `<a href="/jewellery/collections/lab"><img class="opti-new-jewellery-image" src="https://cdn.optimizely.com/img/24400620820/404fbc9f05674ba4a578d039bdc2409e.jpg" alt=""></a>`)
+        }
     }
 });
