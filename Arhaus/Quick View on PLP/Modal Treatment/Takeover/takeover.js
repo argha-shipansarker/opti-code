574715cef830cf0ac6cb061d2ef66fbdd5c916d6
argha-mirinfo <argha@mirinfosys.com>
2025-07-22 18:14:04 +0600

    added

--- Arhaus/Quick View on PLP/Modal Treatment/Takeover/takeover.js
+++ Arhaus/Quick View on PLP/Modal Treatment/Takeover/takeover.js
@@ -31,6 +31,10 @@ utils.observeSelector('.opti-quick-view-modal .quick-view-product .product-image
             product_zoom_image.appendChild(cloned_plp_image);
 
             document.querySelector('.opti-quick-view-modal').style.display = "flex";
+            setTimeout(() => {
+                document.querySelector('.opti-quick-view-modal .quick-view-product').scrollTop = 0;
+            }, 50);
+
             if (!document.querySelector('.opti-quick-view-modal .quick-view-product .opti-close-icon')) {
                 document.querySelector('.opti-quick-view-modal .quick-view-product').insertAdjacentHTML("beforeend", `<div class="opti-close-icon">
     <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
